9dd9fdfde78cef8d375b0d458328184b785ebd6d
Hugo Drumond <hugo.drumond@fe.up.pt>
2017-10-26 21:51:26 +0100

    more code refactor

--- src/main.cpp
+++ src/main.cpp
@@ -30,27 +30,61 @@ constexpr int DEFAULT_HOUGH_LINES_P_THRESHOLD = 83;
 constexpr int MIN_HOUGH_LINES_P_THRESHOLD = 0;
 constexpr int MAX_HOUGH_LINES_P_THRESHOLD = 155;
 
+constexpr double LINES_SELECTION_RADIUS_FACTOR = 0.1;
+
 struct ProgramData {
   Mat image;
+  Mat grayImage;
   int houghCirclesCannyThreshold = DEFAULT_HOUGH_CIRCLES_CANNY_THRESHOLD;
   int houghCirclesAccumulatorThreshold =
       DEFAULT_HOUGH_CIRCLES_ACCUMULATOR_THRESHOLD;
   int houghLinesPThreshold = DEFAULT_HOUGH_LINES_P_THRESHOLD;
 };
 
+struct Circle {
+  Point center;
+  int radius;
+
+  explicit Circle(Vec3f &circleData) {
+    center.x = cvRound(circleData[0]);
+    center.y = cvRound(circleData[1]);
+    radius = cvRound(circleData[2]);
+  }
+};
+
+struct Line {
+  Point a;
+  Point b;
+
+  Line() = default;
+
+  explicit Line(Vec4i rawLine) {
+    a.x = rawLine[0];
+    a.y = rawLine[1];
+    b.x = rawLine[2];
+    b.y = rawLine[3];
+  }
+};
+
 string readCommandLine(int argc, char **argv, string const &defaultImagePath);
 void readImage(string &imagePath, ProgramData &programData);
 void buildGui(TrackbarCallback callback, ProgramData &programData);
 void clockTimeDetector(int, void *);
-vector<Vec4i> naiveClockHandLinesMerge(vector<Vec4i> &clock_lines);
-void swapPoints(Vec4i &line);
+vector<Circle> getCircles(ProgramData &programData);
+vector<Line> getLines(Mat &src, Mat &result, ProgramData &programData);
+void isolateClock(Circle &clockCircle, Mat &image, Mat &clock);
+vector<Line> selectLinesCloseToCircleCenter(vector<Line> &lines, Circle &circle, double radiusFactor);
+vector<Line> naiveClockHandLinesMerge(vector<Line> &clockLines);
+
 double angleBetweenTwoLines(const Point &vec1, const Point &vec2);
-void convertToGray(Mat &src, Mat &dst);
-template <class T>
-constexpr const T &clamp(const T &v, const T &lo, const T &hi);
+void bgr2gray(Mat &src, Mat &dst);
+void gray2bgr(Mat &src, Mat &dst);
 void normalizeHoughCirclesCannyThreshold(int &value);
 void normalizeHoughCirclesAccumulatorThreshold(int &value);
 void normalizeHoughLinesPThreshold(int &value);
+void swapPoints(Line &line);
+template <class T>
+constexpr const T &clamp(const T &v, const T &lo, const T &hi);
 
 int main(int argc, char **argv) {
   string imagePath = readCommandLine(argc, argv, DEFAULT_IMAGE_PATH);
@@ -61,165 +95,131 @@ int main(int argc, char **argv) {
   waitKey(0);
 }
 
-void clockTimeDetector(int, void *rawprogramData) {
-  ProgramData *programData = static_cast<ProgramData *>(rawprogramData);
+void clockTimeDetector(int, void *rawProgramData) {
+  auto *programData = static_cast<ProgramData *>(rawProgramData);
 
-  Mat grayImage;
-  convertToGray(programData->image, grayImage);
-
-  // Reduce the noise so we avoid false circle detection
-  GaussianBlur(grayImage, grayImage, Size(9, 9), 2, 2);
-
-  std::vector<Vec3f> circles;
-
-  HoughCircles(grayImage, circles, HOUGH_GRADIENT,
-               1,  //
-               grayImage.rows / 8, programData->houghCirclesCannyThreshold,
-               programData->houghCirclesAccumulatorThreshold, 0,
-               0  // change the last two parameters (min radius and max radios
-                  // ) to detect large circles
-  );
-
-  // clone the colour, input image for displaying purposes
-  Mat display, clock;  // = src.clone();
-  if (circles.size() < 1) {
+  vector<Circle> circles = getCircles(*programData);
+  if (circles.empty()) {
     return;
   }
-  // ------------------------------------------------------------------
-  Point center(cvRound(circles[0][0]), cvRound(circles[0][1]));
-  int radius = cvRound(circles[0][2]);
-  // circle center
-  // circle(display, center, 3, Scalar(0, 255, 0), -1, 8, 0);
-  // circle outline
-  // circle(display, center, radius, Scalar(0, 0, 255), 3, 8, 0);
 
-  // remove background and isolate clock
-  // center and radius are the results of HoughCircle
-  // mask is a CV_8UC1 image with 0
-  cv::Mat mask =
-      cv::Mat::zeros(programData->image.rows, programData->image.cols, CV_8UC1);
-  circle(mask, center, radius, Scalar(255, 255, 255), -1, 8, 0);
-  programData->image.copyTo(clock,
-                            mask);  // copy values of img to dst if mask is > 0.
-  // ------------------------------------------------------------------
+  Circle clockCircle = circles[0];
 
-  // cv::Mat roi( display, cv::Rect( center.x-radius, center.y-radius, radius*2,
-  // radius*2 ) );
-  // detect hour and minut pointers
-  // pre process
-  Mat src_gray, src_gray_blured, pre_processed_img;
-  // Pass the RGB image to grayScale
-  cvtColor(clock, src_gray, COLOR_RGB2GRAY);
+  Mat clock;
+  isolateClock(clockCircle, programData->image, clock);
+  Mat grayClock;
+  bgr2gray(clock, grayClock);
 
-  // OPTIONAL (bluring image to process only the more linear edges)
-  bilateralFilter(src_gray, src_gray_blured, 25, 150, BORDER_DEFAULT);
+  Mat display;
+  vector<Line> lines = getLines(grayClock, display, *programData);
+  gray2bgr(display, display);
 
-  // Apply Canny edge detector (is requeired to first apply a edge detection
-  // algoritm)
-  Canny(src_gray_blured, pre_processed_img, 50, 200, 3);
+  vector<Line> clockLines =  selectLinesCloseToCircleCenter(lines, clockCircle, LINES_SELECTION_RADIUS_FACTOR);
 
-  // detect
-  vector<Vec4i> p_lines;
-  // show binary image
-  cvtColor(pre_processed_img, display, COLOR_GRAY2BGR);
+  vector<Line> mergedClockLines = naiveClockHandLinesMerge(clockLines);
 
-  /// 2. Use Probabilistic Hough Transform
-  HoughLinesP(pre_processed_img, p_lines, 1, CV_PI / 180,
-              programData->houghLinesPThreshold, 30, 10);
+  cout << mergedClockLines.size() << endl;
 
-  // 1º filtering the vector lines (selecting only the lines near the center)
-
-  // 1º ver 2 pontos da linha perto do centro and organize them (remove the rest
-  // and the ones that stay are organized from center to limit of the clock
-  // hand)
-  double clock_radius_limit = 0.1 * static_cast<double>(radius);
-  vector<Vec4i> clock_lines;
-
-  for (size_t x = 0; x < p_lines.size(); x++) {
-    Vec4i line = p_lines[x];
-
-    Point p1 = Point(line[0], line[1]);
-    Point p2 = Point(line[2], line[3]);
-
-    Point vec1 = p1 - center;
-    Point vec2 = p2 - center;
-
-    if (norm(vec1) <= clock_radius_limit && norm(vec2) > clock_radius_limit) {
-      clock_lines.push_back(line);
-    }
-    if (norm(vec2) <= clock_radius_limit && norm(vec1) > clock_radius_limit) {
-      swapPoints(line);
-      clock_lines.push_back(line);
-    }
+  Line line0;
+  Line line1;
+  if (!mergedClockLines.empty()) {
+    line0 = mergedClockLines[0];
+    line(display, line0.a, line0.b, Scalar(0, 0, 255), 3, LINE_AA);
   }
+  if (mergedClockLines.size() > 1) {
+    line1 = mergedClockLines[1];
 
-  // ver angulo entre eles e depois juntar linhas com angulos parecidos
-  // calculating angle between them (5 degres for exemple or less)
-
-  // merge lines
-  vector<Vec4i> merged_clock_lines = naiveClockHandLinesMerge(clock_lines);
-
-  /// Show the result
-  cout << merged_clock_lines.size() << endl;
-
-  Point p1_1, p1_2, p2_1, p2_2;
-  if (merged_clock_lines.size() > 0) {
-    Vec4i l1 = merged_clock_lines[0];
-
-    p1_1 = Point(l1[0], l1[1]);
-    p1_2 = Point(l1[2], l1[3]);
-    line(display, p1_1, p1_2, Scalar(0, 0, 255), 3, LINE_AA);
-  }
-  if (merged_clock_lines.size() > 1) {
-    Vec4i l2 = merged_clock_lines[1];
-
-    p2_1 = Point(l2[0], l2[1]);
-    p2_2 = Point(l2[2], l2[3]);
-
-    Point vec1 = p1_1 - p1_2;
-    Point vec2 = p2_1 - p2_2;
+    Point vec1 = line0.a - line0.b;
+    Point vec2 = line1.a - line1.b;
 
     cout << "vec1: " << vec1 << ", vec2: " << vec2
          << ",  ang: " << angleBetweenTwoLines(vec1, vec2) << endl;
-    line(display, p2_1, p2_2, Scalar(0, 255, 0), 3, LINE_AA);
+    line(display, line1.a, line1.b, Scalar(0, 255, 0), 3, LINE_AA);
   }
-  // shows the results
-  // line(display, center, Point(center.x + radius,center.y), Scalar(0, 255, 0),
-  // 3, LINE_AA);
+
   imshow(WINDOW_NAME, display);
 }
 
-vector<Vec4i> naiveClockHandLinesMerge(vector<Vec4i> &clock_lines) {
-  for (size_t x = 0; x < clock_lines.size() - 1; x++) {
-    Vec4i l1 = clock_lines[x];
+vector<Circle> getCircles(ProgramData &programData) {
+  Mat blurredImage;
+  GaussianBlur(programData.grayImage, blurredImage, Size(9, 9), 2, 2);
 
-    Point p1_1 = Point(l1[0], l1[1]);
-    Point p1_2 = Point(l1[2], l1[3]);
+  std::vector<Vec3f> raw_circles;
 
-    Point vec1 = p1_1 - p1_2;
+  HoughCircles(blurredImage, raw_circles, HOUGH_GRADIENT, 1, blurredImage.rows / 8, programData.houghCirclesCannyThreshold,
+               programData.houghCirclesAccumulatorThreshold, 0, 0);
 
-    for (size_t y = x + 1; y < clock_lines.size(); y++) {
-      Vec4i l2 = clock_lines[y];
+  vector<Circle> circles;
+  for (auto &raw_circle : raw_circles) {
+    circles.push_back(Circle(raw_circle));
+  }
 
-      Point p2_1 = Point(l2[0], l2[1]);
-      Point p2_2 = Point(l2[2], l2[3]);
+  return circles;
+}
 
-      Point vec2 = p2_1 - p2_2;
+vector<Line> getLines(Mat &src, Mat &result, ProgramData &programData) {
+  bilateralFilter(src, result, 25, 150, BORDER_DEFAULT);
+  Canny(result, result, 50, 200, 3);
+  vector<Vec4i> rawLines;
+  HoughLinesP(result, rawLines, 1, CV_PI / 180, programData.houghLinesPThreshold, 30, 10);
+  vector<Line> lines;
+  for (auto &rawLine : rawLines) {
+    lines.push_back(Line(rawLine));
+  }
+  return lines;
+}
+
+void isolateClock(Circle &clockCircle, Mat &image, Mat &clock) {
+  cv::Mat mask = cv::Mat::zeros(image.rows, image.cols, CV_8UC1);
+  circle(mask, clockCircle.center, clockCircle.radius, Scalar(255, 255, 255), -1, LINE_8, 0);
+  image.copyTo(clock, mask);
+}
+
+vector<Line> selectLinesCloseToCircleCenter(vector<Line> &lines, Circle &circle, double radiusFactor) {
+  double clock_radius_limit = radiusFactor * static_cast<double>(circle.radius);
+  vector<Line> clockHandLines;
+
+  for (auto &line : lines) {
+    Point vec1 = line.a - circle.center;
+    Point vec2 = line.b - circle.center;
+
+    if (norm(vec1) <= clock_radius_limit && norm(vec2) > clock_radius_limit) {
+      clockHandLines.push_back(line);
+    }
+    if (norm(vec2) <= clock_radius_limit && norm(vec1) > clock_radius_limit) {
+      swapPoints(line);
+      clockHandLines.push_back(line);
+    }
+  }
+
+  return clockHandLines;
+}
+
+vector<Line> naiveClockHandLinesMerge(vector<Line> &clockLines) {
+  for (size_t x = 0; x < clockLines.size() - 1; x++) {
+    Line l1 = clockLines[x];
+
+    Point vec1 = l1.a - l1.b;
+
+    for (size_t y = x + 1; y < clockLines.size(); y++) {
+      Line l2 = clockLines[y];
+
+      Point vec2 = l2.a - l2.b;
 
       double ang = angleBetweenTwoLines(vec1, vec2);
       if (ang < 5.0) {
         double dist1 = norm(vec1);
         double dist2 = norm(vec2);
         if (dist1 < dist2) {
-          clock_lines.erase(clock_lines.begin() + x);
+          clockLines.erase(clockLines.begin() + x);
         } else {
-          clock_lines.erase(clock_lines.begin() + y);
+          clockLines.erase(clockLines.begin() + y);
         }
-        return naiveClockHandLinesMerge(clock_lines);
+        return naiveClockHandLinesMerge(clockLines);
       }
     }
   }
-  return clock_lines;
+  return clockLines;
 }
 
 double angleBetweenTwoLines(const Point &vec1, const Point &vec2) {
@@ -230,9 +230,8 @@ double angleBetweenTwoLines(const Point &vec1, const Point &vec2) {
   return (ang * 180.0) / M_PI;
 }
 
-void swapPoints(Vec4i &line) {
-  swap(line[0], line[2]);
-  swap(line[1], line[3]);
+void swapPoints(Line &line) {
+  swap(line.a, line.b);
 }
 
 string readCommandLine(int argc, char **argv, string const &defaultImagePath) {
@@ -245,6 +244,7 @@ string readCommandLine(int argc, char **argv, string const &defaultImagePath) {
 
 void readImage(string &imagePath, ProgramData &programData) {
   programData.image = imread(imagePath, IMREAD_COLOR);
+  bgr2gray(programData.image, programData.grayImage);
 
   if (programData.image.empty()) {
     throw std::invalid_argument("Invalid Input Image");
@@ -284,4 +284,6 @@ constexpr const T &clamp(const T &v, const T &lo, const T &hi) {
   return std::max(lo, std::min(v, hi));
 }
 
-void convertToGray(Mat &src, Mat &dst) { cvtColor(src, dst, COLOR_BGR2GRAY); }
+void bgr2gray(Mat &src, Mat &dst) { cvtColor(src, dst, COLOR_BGR2GRAY); }
+
+void gray2bgr(Mat &src, Mat &dst) { cvtColor(src, dst, COLOR_GRAY2BGR); }
