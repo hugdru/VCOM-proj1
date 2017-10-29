f81ab404a0c69002ef5f157a97ae903ecf70f653
Hugo Drumond <hugo.drumond@fe.up.pt>
2017-10-29 15:17:55 +0000

    cmake fixed opencv folder location in windows

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 #include <cmath>
 #include <iostream>
 
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/imgproc.hpp"
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace std;
 using namespace cv;
